2707ed6adcb1bffa8170830d0c11f674b69e3956
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-11 21:24:59 +0530

    added users, clubs, events

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,18 +1,25 @@
 import * as mongoDB from "mongodb";
 
-export const collections: { games?: mongoDB.Collection } = {};
+export const collections: { games?: mongoDB.Collection, users?: mongoDB.Collection, events?: mongoDB.Collection, clubs?: mongoDB.Collection } = {};
 
 export async function connectToDatabase() {
     try {
-        const client: mongoDB.MongoClient = new mongoDB.MongoClient("mongodb://localhost:27017/gamesDB");
+        const client: mongoDB.MongoClient = new mongoDB.MongoClient("mongodb://localhost:27017/clubcoord");
         await client.connect();
         
-        const db: mongoDB.Db = client.db("gamesDB");
+        const db: mongoDB.Db = client.db("clubcoord");
+        
         const gamesCollection: mongoDB.Collection = db.collection("games");
+        const usersCollection: mongoDB.Collection = db.collection("users");
+        const eventsCollection: mongoDB.Collection = db.collection("events");
+        const clubsCollection: mongoDB.Collection = db.collection("clubs");
         
         collections.games = gamesCollection;
+        collections.users = usersCollection;
+        collections.events = eventsCollection;
+        collections.clubs = clubsCollection;
         
-        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
+        console.log(`Successfully connected to database: ${db.databaseName} and collections: ${gamesCollection.collectionName}, ${usersCollection.collectionName}, ${eventsCollection.collectionName}, ${clubsCollection.collectionName}`);
     } catch (error) {
         console.error("Error connecting to database:", error);
         throw error; // Rethrow the error to handle it at a higher level
